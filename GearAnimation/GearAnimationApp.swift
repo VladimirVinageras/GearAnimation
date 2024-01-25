ef226ef71a14928167b2de37dff6879d30030deb
Vladimir Vinakheras <vvinageras@gmail.com>
2024-01-25 18:05:42 +0200

    Initial Commit

---
+++ GearAnimation/GearAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GearAnimationApp.swift
+//  GearAnimation
+//
+//  Created by Vladimir Vinakheras on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GearAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
